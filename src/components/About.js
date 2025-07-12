02913a3196207a6167d65a34148aea690ada0d15
Mohamed Abdulrashid <mohamedabdul9977@gmail.com>
2025-07-12 19:54:22 +0300

    Complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import user from "../data/user"; 
+import Links from "./Links";
 
-function About() {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {bio ? <p>{bio}</p> : null}
+      <img src={user.image} alt="I made this" />
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
--- src/data/user.js
+++ src/data/user.js
@@ -3,6 +3,7 @@ const user = {
   city: "New York",
   bio: "I made this!",
   color: "firebrick",
+  image: "https://i.imgur.com/mV8PQxj.gif",
   links: {
     github: "https://github.com/liza",
     linkedin: "https://www.linkedin.com/in/liza/",
